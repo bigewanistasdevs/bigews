1e0be9fdb20b7db5b3f084b4b7fa7f69ab9a7cc2
Alison Lovegood <alisondlovegood@gmail.com>
2021-03-03 17:08:57 -0300

    controles, e rota, iniciais para teste

--- backend/app/Http/Controllers/api/TarefaController.php
+++ backend/app/Http/Controllers/api/TarefaController.php
@@ -4,82 +4,39 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tarefa;
 
 class TarefaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    // rota principal like?? n entendi praq mas ta ai
     public function index()
     {
-        //
+        return Tarefa::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // em teoria cria novas Tarefas
     public function store(Request $request)
     {
-        //
+        Tarefa::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // retorna os objetos criados
     public function show($id)
     {
-        //
+        return Tarefa::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    // update é update, muda blablabla
     public function update(Request $request, $id)
     {
-        //
+        $tarefa = Tarefa::findOrFail($id);
+        $tarefa->update($request->all());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $tarefa = Tarefa::findOrFail($id);
+        $tarefa->delete();
     }
 }
