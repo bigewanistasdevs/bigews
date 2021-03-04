22230b5ed182564b77d3688940117df9b4ece053
Alison Lovegood <alisondlovegood@gmail.com>
2021-03-03 22:02:39 -0300

    teste

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,4 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 Route::apiResource('listas', 'api\ListaController');
 Route::apiResource('tarefas', 'api\TarefaController');
